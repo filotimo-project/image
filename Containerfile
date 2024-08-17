dae5476fe308b8675647e0035005a671c60568f9
Thomas Duckworth <tduck973564@gmail.com>
2024-08-17 22:56:15 +1000

    initial

--- Containerfile
+++ Containerfile
@@ -15,7 +15,7 @@
 # - "base"
 #
 #  "aurora", "bazzite", "bluefin" or "ucore" may also be used but have different suffixes.
-ARG SOURCE_IMAGE="silverblue"
+ARG SOURCE_IMAGE="kinoite"
 
 ## SOURCE_SUFFIX arg should include a hyphen and the appropriate suffix name
 # These examples all work for silverblue/kinoite/sericea/onyx/lazurite/vauxite/base
@@ -36,13 +36,43 @@ ARG SOURCE_IMAGE="silverblue"
 ARG SOURCE_SUFFIX="-main"
 
 ## SOURCE_TAG arg must be a version built for the specific image: eg, 39, 40, gts, latest
-ARG SOURCE_TAG="latest"
+ARG SOURCE_TAG="40"
 
 
 ### 2. SOURCE IMAGE
 ## this is a standard Containerfile FROM using the build ARGs above to select the right upstream image
 FROM ghcr.io/ublue-os/${SOURCE_IMAGE}${SOURCE_SUFFIX}:${SOURCE_TAG}
+FROM ghcr.io/ublue-os/fsync-kernel:${FEDORA_MAJOR_VERSION} AS fsync
 
+# fsync kernel
+RUN --mount=type=cache,dst=/var/cache/rpm-ostree \
+    --mount=type=bind,from=fsync,src=/tmp/rpms,dst=/tmp/fsync-rpms \
+    rpm-ostree cliwrap install-to-root / && \
+    if [[ "${KERNEL_FLAVOR}" =~ "fsync" ]]; then \
+        echo "Will install ${KERNEL_FLAVOR} kernel" && \
+        rpm-ostree override replace \
+        --experimental \
+            /tmp/fsync-rpms/kernel-[0-9]*.rpm \
+            /tmp/fsync-rpms/kernel-core-*.rpm \
+            /tmp/fsync-rpms/kernel-modules-*.rpm \
+            /tmp/fsync-rpms/kernel-uki-virt-*.rpm \
+    ; else \
+        echo "will use kernel from ${KERNEL_FLAVOR} images" \
+    ; fi && \
+    rpm-ostree install \
+        scx-scheds && \
+    /usr/libexec/containerbuild/cleanup.sh && \
+    ostree container commit
+
+
+# Install important repos
+RUN --mount=type=cache,dst=/var/cache/rpm-ostree \
+    curl -Lo /etc/yum.repos.d/filotimo.repo https://download.opensuse.org/repositories/home:/tduck:/filotimolinux/Fedora_${FEDORA_MAJOR_VERSION}/home:tduck:filotimolinux.repo && \
+    curl -Lo /etc/yum.repos.d/klassy.repo https://mirrorcache-au.opensuse.org/repositories/home:/paul4us/Fedora_${FEDORA_MAJOR_VERSION}/home:paul4us.repo && \
+    curl -Lo /etc/yum.repos.d/terra.repo https://terra.fyralabs.com/terra.repo && \
+    rpm-ostree install terra-release rpmfusion-free-tainted rpmfusion-nonfree-tainted && \
+    /usr/libexec/containerbuild/cleanup.sh && \
+    ostree container commit
 
 ### 3. MODIFICATIONS
 ## make modifications desired in your image and install packages by modifying the build.sh script
--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -12,12 +11,98 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+# Install Filotimo packages
+rpm-ostree override replace --experimental \
+filotimo-environment \
+filotimo-kde-overrides \
+msttcore-fonts-installer \
+onedriver \
+appimagelauncher \
+filotimo-backgrounds \
+filotimo-grub-theme \
+filotimo-plymouth-theme \
+filotimo-branding \
+filotimo-atychia
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# Remove misc. packages
+rpm-ostree override remove \
+power-profiles-daemon \
+firefox \
+mediawriter \
+krusader \
+konversation \
+k3b \
+kontact \
+kmail \
+korganizer \
+kaddressbook \
+*akonadi* \
+mariadb* \
+kmines \
+kmahjongg \
+kpat
 
-#### Example for enabling a System Unit File
+# Install misc. packages
+rpm-ostree override replace --experimental \
+tuned tuned-ppd \
+distrobox \
+git \
+vlc \
+kdenetwork-filesharing \
+ark \
+kio-admin \
+kleopatra \
+firewall-config \
+setroubleshoot \
+firewall-config \
+openssl openssl-libs \
+python3-pip \
+nmap \
+p7zip \
+p7zip-plugins \
+unzip \
+unrar \
+libheif libheif-tools \
+gstreamer1-plugin-openh264 \
+mesa-vdpau-drivers-freeworld mesa-va-drivers-freeworld \
+intel-media-driver libva-media-driver libva-utils vdpauinfo \
+nvidia-vaapi-driver \
+libdvdcss \
+ffmpeg \
+epson-inkjet-printer-escpr2 \
+foomatic-db \
+gutenprint \
+hplip
 
-systemctl enable podman.socket
+FLATPAK_FLATHUB=( org.mozilla.Firefox
+org.mozilla.Thunderbird
+org.kde.isoimagewriter
+org.kde.kclock
+org.kde.kweather
+org.kde.francis
+com.github.wwmm.easyeffects
+org.kde.skanpage
+org.kde.kamoso
+org.kde.elisa
+org.kde.kolourpaint
+org.kde.digikam
+org.kde.kget
+org.kde.ktorrent
+org.kde.krecorder )
+
+# Add flathub
+flatpak remote-add --if-not-exists -y flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-modify --enable -y flathub
+
+for app in ${FLATPAK_FLATHUB[@]}; do
+	flatpak install -y flathub "$app"
+done
+
+flatpak override --socket=wayland org.mozilla.Thunderbird
+flatpak override --env=MOZ_ENABLE_WAYLAND=1 org.mozilla.Thunderbird
+
+# Enable samba for filesharing
+systemctl enable smbd
+
+# Mask hibernate - usually just causes problems
+systemctl mask hibernate.target

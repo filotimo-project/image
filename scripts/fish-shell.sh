7873c15fae877607107266a30d05a2e935d01337
Thomas Duckworth <tduck973564@gmail.com>
2024-09-25 23:32:39 +1000

    hide fish desktop entry

--- scripts/fish-shell.sh
+++ scripts/fish-shell.sh
@@ -20,3 +20,6 @@ sed -i '/\/bin\/fish/d' /etc/shells
 
 # Remove the intro message, we already have user-motd
 echo 'set -U fish_greeting ""' >> /usr/share/fish/config.fish
+
+# Hide the desktop entry
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/fish.desktop
